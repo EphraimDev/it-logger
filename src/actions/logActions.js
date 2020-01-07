e2f3853a58f1c5650cdb3fd8a546e1585af6e1eb
Ephraimdev <aigbefoephraim@yahoo.com>
2020-01-07 18:33:18 +0100

    added search feature

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   SET_CURRENT,
   CLEAR_CURRENT,
-  UPDATE_LOG
+  UPDATE_LOG,
+  SEARCH_LOGS
 } from './types';
 
 //  Get logs from server
@@ -57,29 +58,49 @@ export const addLog = log => async dispatch => {
 
 // Update log on server
 export const updateLog = log => async dispatch => {
-    try {
-      setLoading();
-  
-      const res = await fetch(`/logs/${log.id}`, {
-        method: 'PUT',
-        body: JSON.stringify(log),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      const data = await res.json();
-  
-      dispatch({
-        type: UPDATE_LOG,
-        payload: data
-      });
-    } catch (err) {
-      dispatch({
-        type: LOGS_ERROR,
-        payload: err.response.data
-      });
-    }
-  };
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+//  Get logs from server
+export const searchLogs = (text) => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
 
 //  Delete log from server
 export const deleteLog = id => async dispatch => {
